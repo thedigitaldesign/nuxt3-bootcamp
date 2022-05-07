18fa82b40482b386ec12ea1d05c1fd16cf07f0e7
Josh(ua) Christensen <joshua@codingwithjosh.com>
2022-05-07 15:17:15 -0500

    Created scripts to build project with empty README files

--- 01-Baby-Name-Generator/nuxt.config.ts
+++ 01-Baby-Name-Generator/nuxt.config.ts
@@ -2,5 +2,15 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+        }
+      }
+    }
+  },
+  css: [`~/assets/css/tailwind.css`]
 })
